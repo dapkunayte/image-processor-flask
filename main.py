b70c5aabec448c268d0d94eeb6e3a254a9431fa0
Borovskoy Aleksey <gladiatormahotina@yandex.ru>
2023-01-17 20:31:35 +0700

    update html templates, add remove background, add font size, font color and background color setting to text_to_image

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, request, redirect, flash, render_template, send_from_directory, url_for
 from werkzeug.utils import secure_filename
 from PIL import Image, ImageDraw, ImageFont
+from rembg.bg import remove
+import io
 
 UPLOAD_FOLDER = 'imgs'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
@@ -26,10 +28,11 @@ def black_and_white_dithering(input_image_path, output_image_path, dithering=Tru
     bw.save(output_image_path)
 
 
-def text_to_image_draw(output_image_path, input_text, background_color, text_color):
+def text_to_image_draw(output_image_path, input_text, background_color, text_color, text_size):
     im = Image.new('RGB', (200, 200), color=background_color)
+    font = ImageFont.truetype("times.ttf", size=int(text_size))
     draw_text = ImageDraw.Draw(im)
-    draw_text.text((100, 100), input_text, fill=text_color)
+    draw_text.text((100, 100), input_text, fill=text_color, font=font)
     im.save(output_image_path)
 
 
@@ -38,11 +41,23 @@ def watermark_photo(input_image_path, output_image_path, watermark_image_path, p
     watermark = Image.open(watermark_image_path)
     width, height = base_image.size
     # add watermark to your image
-    transparent = Image.new('RGB', (width, height), (0, 0, 0, 0))
+    transparent = Image.new('RGBA', (width, height), (0, 0, 0, 0))
     transparent.paste(base_image, (0, 0))
     transparent.paste(watermark, position, mask=watermark)
-    transparent.show()
     transparent.save(output_image_path)
+
+
+def remove_background(input_image_path, output_image_path):
+    base_image = Image.open(input_image_path)
+    base_image.save(input_image_path)
+    bg_image = remove(base_image)
+    bg_image.save(output_image_path)
+
+
+def to_png(input_filename):
+    output_filename = input_filename.replace(".jpeg", ".png")
+    output_filename = input_filename.replace(".jpg", ".png")
+    return output_filename
 
 
 @app.route('/')
@@ -68,7 +83,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             # безопасно извлекаем оригинальное имя файла
             filename = secure_filename(file.filename)
-            bw_filename = "bw_"+filename
+            bw_filename = "bw_"+to_png(filename)
             # сохраняем файл
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             black_and_white_dithering('imgs/'+filename, 'imgs/'+bw_filename)
@@ -88,8 +103,9 @@ def txt_to_img():
         text_data = request.form['text']
         background_color = request.form['background_color']
         text_color = request.form['text_color']
+        text_size = request.form['text_size']
         filename = "image.png"
-        text_to_image_draw('imgs/'+filename, text_data, background_color, text_color)
+        text_to_image_draw('imgs/'+filename, text_data, background_color, text_color, text_size)
         return redirect(url_for('download_file', name=filename))
     return render_template('text_to_image.html')
 
@@ -106,7 +122,7 @@ def add_img_watermark():
         file2 = request.files['file2']
         # Если файл не выбран, то браузер может
         # отправить пустой файл без имени.
-        if file1.filename == '' or file2.filename== '':
+        if file1.filename == '' or file2.filename == '':
             flash('Нет выбранного файла')
             return redirect(request.url)
         if file1 and allowed_file(file1.filename) and file2 and allowed_file(file2.filename):
@@ -115,13 +131,46 @@ def add_img_watermark():
             filename2 = secure_filename(file2.filename)
             file1.save(os.path.join(app.config['UPLOAD_FOLDER'], filename1))
             file2.save(os.path.join(app.config['UPLOAD_FOLDER'], filename2))
-            wm_filename = "wm_"+filename1
+            wm_filename = "wm_"+to_png(filename1)
             watermark_photo('imgs/'+filename1, 'imgs/'+wm_filename, 'imgs/'+filename2, position=(0,0))
             return redirect(url_for('download_file', name=wm_filename))
         elif not(allowed_file(file1.filename)) or not(allowed_file(file2.filename)):
             flash('Некорректное расширение файла. Доступны: .png, .jpeg, .jpg')
             return redirect(request.url)
     return render_template('watermark.html')
+
+
+@app.route('/remove_bg', methods=["GET", "POST"])
+def remove_bg():
+    if request.method == 'POST':
+        # проверим, передается ли в запросе файл
+        if 'file' not in request.files:
+            # После перенаправления на страницу загрузки
+            # покажем сообщение пользователю
+            flash('Не могу прочитать файл')
+            return redirect(request.url)
+        file = request.files['file']
+        # Если файл не выбран, то браузер может
+        # отправить пустой файл без имени.
+        if file.filename == '':
+            flash('Нет выбранного файла')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            # безопасно извлекаем оригинальное имя файла
+            filename = secure_filename(file.filename)
+            filename = filename.replace(".jpg",".png")
+            bg_filename = "bg_" + to_png(filename)
+            # сохраняем файл
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            remove_background('imgs/' + filename, 'imgs/' + bg_filename)
+            # если все прошло успешно, то перенаправляем
+            # на функцию-представление `download_file`
+            # для скачивания файла
+            return redirect(url_for('download_file', name=bg_filename))
+        elif not (allowed_file(file.filename)):
+            flash('Некорректное расширение файла. Доступны: .png, .jpeg, .jpg')
+            return redirect(request.url)
+    return render_template('background.html')
 
 
 @app.route('/downloaded/<name>')
